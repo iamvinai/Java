5eae2ec15111541aafff96f83b74232a308502d7
Vinai <vinaiinfo18@gmail.com>
2024-08-23 20:18:18 -0500

    HashMapComparision and CustomComparatorUsingPairsForJava

---
+++ CustomComparatorUsingPairsForJava.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+public class CustomComparatorUsingPairsForJava {
+
+    public static void main(String[] args) {
+
+        // Initialize a HashMap with some sample data
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Apple", 3);
+        map.put("Banana", 2);
+        map.put("Cherry", 5);
+        map.put("Date", 2);
+        map.put("Elderberry", 3);
+
+        // Convert the map's entry set into a List of Map.Entry
+        List<Map.Entry<String, Integer>> entryList = new ArrayList<>(map.entrySet());
+
+        // Create a comparator to sort by value first, then by key
+        Comparator<Map.Entry<String, Integer>> valueThenKeyComparator = new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
+                // Compare by value first
+                int valueComparison = Integer.compare(e1.getValue(), e2.getValue());
+                // If values are equal, compare by key
+                if (valueComparison == 0) {
+                    return e1.getKey().compareTo(e2.getKey());
+                }
+                return valueComparison;
+            }
+        };
+
+        // Sort the list of entries using the combined comparator
+        entryList.sort(valueThenKeyComparator);
+
+        // Create a LinkedHashMap to maintain the sorted order
+        Map<String, Integer> sortedMap = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> entry : entryList) {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+
+        // Output the sorted map
+        System.out.println("Sorted Map:");
+        for (Map.Entry<String, Integer> entry : sortedMap.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+
+        // Create a list of AbstractMap.SimpleEntry pairs 
+        // This is a parallel for using pairs in C++ and then sorting them using custom compararator.
+        List<Map.Entry<String, Integer>> pairList = new ArrayList<>();
+        pairList.add(new AbstractMap.SimpleEntry<>("Apple", 3));
+        pairList.add(new AbstractMap.SimpleEntry<>("Banana", 2));
+        pairList.add(new AbstractMap.SimpleEntry<>("Cherry", 5));
+        pairList.add(new AbstractMap.SimpleEntry<>("Date", 2));
+        pairList.add(new AbstractMap.SimpleEntry<>("Elderberry", 3));
+
+        // Define a custom comparator to sort pairs by value, then by key
+        Comparator<Map.Entry<String, Integer>> customComparator = new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
+                // Compare by value first
+                int valueComparison = Integer.compare(e1.getValue(), e2.getValue());
+                // If values are equal, compare by key
+                if (valueComparison == 0) {
+                    return e1.getKey().compareTo(e2.getKey());
+                }
+                return valueComparison;
+            }
+        };
+
+        // Sort the list using the custom comparator
+        pairList.sort(customComparator);
+
+        // Output the sorted list
+        System.out.println("Sorted Pairs:");
+        for (Map.Entry<String, Integer> entry : pairList) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+    }
+}
---
+++ HashMapComparision.java
@@ -0,0 +1,55 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Map;
+public class HashMapComparision {
+    public static void main(String args[]) {
+
+        // Initialize and populate HashMaps
+        Map<String, String> hmap1 = new HashMap<String, String>();
+        hmap1.put("India", "New Delhi");
+        hmap1.put("USA", "Washington DC");
+        hmap1.put("Japan", "Tokyo");
+
+        HashMap<String, String> hmap2 = new HashMap<String, String>();
+        hmap2.put("India", "New Delhi");
+        hmap2.put("Japan", "Tokyo");
+        hmap2.put("Japan New", "Tokyo");
+        hmap2.put("USA", "Washington DC");
+
+        HashMap<String, String> hmap3 = new HashMap<String, String>();
+        hmap3.put("India", "New Delhi");
+        hmap3.put("Japan", "Tokyo");
+        hmap3.put("USA", "Washington DC");
+        hmap3.put("China", "Beijing");
+
+        // Compare HashMaps
+        System.out.println(hmap1.equals(hmap2)); // Compare hmap1 and hmap2
+        System.out.println(hmap2.equals(hmap3)); // Compare hmap2 and hmap3
+
+        // Compare key sets of HashMaps
+        System.out.println(hmap1.keySet().equals(hmap2.keySet())); // Compare keys of hmap1 and hmap2
+        System.out.println(hmap2.keySet().equals(hmap3.keySet())); // Compare keys of hmap2 and hmap3
+
+        // Union and difference of key sets
+        HashSet<String> hset = new HashSet<String>(hmap1.keySet());
+        hset.addAll(hmap3.keySet()); // Union of keys from hmap1 and hmap3
+        System.out.println(hset);
+        hset.removeAll(hmap1.keySet()); // Difference of keys from hmap1 and hmap3
+        System.out.println(hset);
+
+        // Compare values of HashMaps - duplicates values are not allowed 
+        ArrayList<String> arr1 = new ArrayList<String>(hmap1.values());
+        ArrayList<String> arr2 = new ArrayList<String>(hmap3.values());
+        ArrayList<String> arr3 = new ArrayList<String>(hmap2.values());
+        System.out.println(arr1.equals(arr2)); // Compare values of hmap1 and hmap3
+        System.out.println(arr1.equals(arr3)); // Compare values of hmap1 and hmap2
+
+        // Compare values of HashMaps - duplicates values are allowed 
+        HashSet<String> hset1 = new HashSet<String>(hmap1.values());
+        HashSet<String> hset2 = new HashSet<String>(hmap3.values());
+        HashSet<String> hset3 = new HashSet<String>(hmap2.values());
+        System.out.println(hset1.equals(hset2)); // Compare values of hmap1 and hmap3
+        System.out.println(hset1.equals(hset3)); // Compare values of hmap1 and hmap2
+    }
+}
