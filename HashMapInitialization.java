6370ebdaa7dc444d71bab79b9c17ee0428aa66c8
Vinai <vinaiinfo18@gmail.com>
2024-08-24 21:33:06 -0500

    HashMapInitialization and MapComparison

---
+++ HashMapInitialization.java
@@ -0,0 +1,46 @@
+import java.util.AbstractMap;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapInitialization {
+
+    // Initializing a mutable HashMap using a static block
+    public static HashMap<String, Integer> marksMap;
+
+    static {
+        marksMap = new HashMap<String, Integer>();
+        marksMap.put("A", 100);
+        marksMap.put("B", 200);
+    }
+
+    public static void main(String args[]) {
+
+        // Initializing a mutable HashMap
+        Map<String, String> hmap1 = new HashMap<String, String>();
+
+        // Accessing an element from the mutable HashMap
+        System.out.println(HashMapInitialization.marksMap.get("A"));
+
+        // Initializing an immutable map with a single entry
+        Map<String, Integer> hmap3 = Collections.singletonMap("A", 100);
+        System.out.println(hmap3.get("A"));
+        // hmap3.put("B", 200); // Unsupported Operation Exception
+
+        // Initializing an empty immutable map
+        hmap1 = Map.of();
+        // hmap1.put("Vinai", "Hi"); // Unsupported Operation Exception
+
+        // Initializing an immutable map with multiple entries
+        hmap1 = Map.of("Vinai", "18", "Abdevilliers", "17");
+        System.out.println(hmap1);
+        // hmap1.put("Dhoni", "7"); // Unsupported Operation Exception
+
+        // Initializing an immutable map using Map.ofEntries
+        hmap1 = Map.ofEntries(
+            new AbstractMap.SimpleEntry<String, String>("Vinai", "18"),
+            new AbstractMap.SimpleEntry<String, String>("Abdevilliers", "17")
+        );
+        System.out.println(hmap1);
+    }
+}
---
+++ MapComparison.java
@@ -0,0 +1,66 @@
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MapComparison {
+
+    public static void main(String[] args) {
+        // HashMap example
+        Map<String, String> hashMap = new HashMap<>();
+        hashMap.put("key1", "value1");
+        hashMap.put("key2", "value2");
+        System.out.println("HashMap: " + hashMap);
+        // HashMap is not synchronized and is not thread-safe
+
+        // Hashtable example
+        Map<String, String> hashtable = new Hashtable<>();
+        hashtable.put("key1", "value1");
+        hashtable.put("key2", "value2");
+        System.out.println("Hashtable: " + hashtable);
+        // Hashtable is synchronized and thread-safe but generally slower
+
+        // SynchronizedMap example
+        Map<String, String> synchronizedMap = Collections.synchronizedMap(new HashMap<>());
+        synchronizedMap.put("key1", "value1");
+        synchronizedMap.put("key2", "value2");
+        System.out.println("SynchronizedMap: " + synchronizedMap);
+        // SynchronizedMap is a synchronized wrapper around a HashMap
+
+        // ConcurrentHashMap example
+        Map<String, String> concurrentMap = new ConcurrentHashMap<>();
+        concurrentMap.put("key1", "value1");
+        concurrentMap.put("key2", "value2");
+        System.out.println("ConcurrentHashMap: " + concurrentMap);
+        // ConcurrentHashMap is designed for concurrent access and is thread-safe
+
+        // Demonstrating differences in thread safety
+        Runnable task = () -> {
+            for (int i = 0; i < 1000; i++) {
+                hashMap.put("key" + i, "value" + i);
+                hashtable.put("key" + i, "value" + i);
+                synchronizedMap.put("key" + i, "value" + i);
+                concurrentMap.put("key" + i, "value" + i);
+            }
+        };
+
+        Thread thread1 = new Thread(task);
+        Thread thread2 = new Thread(task);
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("HashMap size: " + hashMap.size());
+        System.out.println("Hashtable size: " + hashtable.size());
+        System.out.println("SynchronizedMap size: " + synchronizedMap.size());
+        System.out.println("ConcurrentHashMap size: " + concurrentMap.size());
+    }
+}
